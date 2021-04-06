459fd49d79ea07b55aa46084f824d891ded034c3
isaciniciato <idd_iciniciato@uolinc.com>
2021-04-06 15:44:30 -0300

    first commit

---
+++ src/main/java/com/isa/estudos/jpa/salejpa/SaleJpaApplication.java
@@ -0,0 +1,13 @@
+package com.isa.estudos.jpa.salejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaleJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isa/estudos/jpa/salejpa/SaleJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isa.estudos.jpa.salejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
