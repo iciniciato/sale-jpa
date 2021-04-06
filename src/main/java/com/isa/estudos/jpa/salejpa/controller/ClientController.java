995e03864a5e079710579a952c5febb89786676c
isaciniciato <idd_iciniciato@uolinc.com>
2021-04-06 16:20:50 -0300

    add endpoints client

--- src/main/java/com/isa/estudos/jpa/salejpa/controller/ClientController.java
+++ src/main/java/com/isa/estudos/jpa/salejpa/controller/ClientController.java
@@ -5,8 +5,11 @@ import com.isa.estudos.jpa.salejpa.vo.ClientVO;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,8 +26,26 @@ public class ClientController {
         return ResponseEntity.ok(clientService.getClients());
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<ClientVO> getClient(@PathVariable Long id) {
+        return ResponseEntity.ok(clientService.getClientByIndex(id));
+    }
+
     @PostMapping
     public ResponseEntity setClient(@RequestBody ClientVO clientVO) {
         return ResponseEntity.status(HttpStatus.CREATED).body(clientService.createClient(clientVO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity putClient(@RequestBody ClientVO clientVO,
+                                    @PathVariable Long id) {
+        clientService.changeClient(clientVO, id);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteClient(@PathVariable Long id) {
+        clientService.deleteClientByIndex(id);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/isa/estudos/jpa/salejpa/service/ClientService.java
+++ src/main/java/com/isa/estudos/jpa/salejpa/service/ClientService.java
@@ -5,9 +5,12 @@ import com.isa.estudos.jpa.salejpa.repository.ClientRepository;
 import com.isa.estudos.jpa.salejpa.vo.ClientVO;
 import com.isa.estudos.jpa.salejpa.vo.mapper.ClientMapper;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
@@ -21,8 +24,31 @@ public class ClientService {
         return clientMapper.toClientVO(clientRepository.findAll());
     }
 
+    public ClientVO getClientByIndex(Long id) {
+        ClientEntity clientEntity = clientFinderWithException(id);
+        return clientMapper.toClientVO(clientEntity);
+    }
+
     public ClientVO createClient(ClientVO clientVO) {
         ClientEntity clientEntity = clientMapper.toClientEntity(clientVO);
         return clientMapper.toClientVO(clientRepository.save(clientEntity));
+    }
+
+    public void changeClient(ClientVO clientVO, Long id) {
+        ClientEntity client = clientFinderWithException(id);
+        client.setName(clientVO.getName());
+        client.setCpf(clientVO.getCpf());
+        client.setAddress(clientVO.getAddress());
+        clientMapper.toClientVO(clientRepository.save(client));
+    }
+
+    public void deleteClientByIndex(Long id) {
+        Optional<ClientEntity> clientEntity = clientRepository.findById(id);
+        clientEntity.ifPresent(clientRepository::delete);
+    }
+
+    private ClientEntity clientFinderWithException(Long id) {
+        return clientRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Client not found"));
     }
 }
